7638691d1d798d7f6e95ef83259f9389c834d7f6
Enrico <er.enricodoro@gmail.com>
2022-02-06 12:41:46 +0100

    setting up utilities

---
+++ src/components/Icons.tsx
@@ -0,0 +1,93 @@
+function Android(){
+    return <img alt="android" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/android/android-original.svg" />
+}
+
+function Apple(){
+    return <img alt="apple" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/apple/apple-original.svg" />
+}
+
+function Arduino(){
+    return <img alt="arduino" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/arduino/arduino-original.svg" />
+}
+
+function Bootstrap(){
+    return <img alt="bootstrap" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" />
+}
+
+function C(){
+    return <img alt="c" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" />
+}
+
+function CSharp(){
+    return <img alt="csharp" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" />
+}
+
+function CSS(){
+    return <img alt="css" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+}
+
+function Docker(){
+    return <img alt="docker" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg" />
+}
+
+function Figma(){
+    return <img alt="figma" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" />
+}
+
+function Firebase(){
+    return <img alt="firebase" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" />
+}
+
+function Git(){
+    return <img alt="git" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
+}
+
+function HTML(){
+    return <img alt="html" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+}
+
+function Java(){
+    return <img alt="java" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" />
+}
+
+function JavaScript(){
+    return <img alt="javascript" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+}
+
+function Kotlin(){
+    return <img alt="kotlin" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg" />
+}
+
+function Linux(){
+    return <img alt="linux" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" />
+}
+
+function MaterialUI(){
+    return <img alt="mui" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg" />
+}
+
+function Raspberry(){
+    return <img alt="raspberry" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/raspberrypi/raspberrypi-original.svg" />
+}
+
+function React(){
+    return <img alt="react" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+}
+
+function TypeScript(){
+    return <img alt="typescript" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" />
+}
+
+function Ubuntu(){
+    return <img alt="ubuntu" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ubuntu/ubuntu-plain.svg" />
+}
+
+function Unity(){
+    return <img alt="unity" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/unity/unity-original.svg" />
+}
+
+function Windows(){
+    return <img alt="windows" height={64} width={64} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/windows8/windows8-original.svg" />
+}
+
+export { Android, Apple, Arduino, Bootstrap, C, CSharp, CSS, Docker, Figma, Firebase, Git, HTML, Java, JavaScript, Kotlin, Linux, MaterialUI, Raspberry, React, TypeScript, Ubuntu, Unity, Windows }
